4a05004024ebc41798e26e5f9084d26fea7a7faf
zhangpei <peigong.dh@qq.com>
2017-07-24 14:45:00 +0800

    fix register bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,11 @@
                 <div class="panel-heading">控制面板</div>
 
                 <div class="panel-body">
+                    @if(user()->is_active)
                     您已登录！
+                    @else
+                    请验证邮箱
+                    @endif
                 </div>
             </div>
         </div>
