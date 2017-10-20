a0b7764f8113888fce33a029d0c25b42ae737df8
peigongDH <peigong.dh@qq.com>
2017-10-20 23:05:23 +0800

    fix bug when user register auto login without email verify

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">控制面板</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">控制面板</div>
 
-                <div class="panel-body">
-                    @if(user()->is_active)
-                    您已登录！
-                    @else
-                    请验证邮箱
-                    @endif
+                    <div class="panel-body">
+                        @if(user()->is_active)
+                            您已登录！
+                        @else
+                            请验证邮箱
+                            {{ Auth::logout() }}
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
